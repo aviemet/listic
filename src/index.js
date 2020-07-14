2b1b5cf32cf2a50c81fbd64091d91ce1773f68cb
Avram Walden <avram@thebatterysf.com>
2020-07-14 10:30:15 -0700

    First commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import * as firebase from 'firebase'
+import ReactDOM from 'react-dom'
+import App from './App'
+import * as serviceWorker from './serviceWorker'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDin-xOT8bMoecdkDCIbWvlg43UtfASgzA",
+  authDomain: "listic-17cbd.firebaseapp.com",
+  databaseURL: "https://listic-17cbd.firebaseio.com",
+  projectId: "listic-17cbd",
+  storageBucket: "listic-17cbd.appspot.com",
+  messagingSenderId: "906805463498",
+  appId: "1:906805463498:web:658f80e57fcefd6af0e733",
+  measurementId: "G-CZP1EDG0LF"
+}
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig)
+firebase.analytics()
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
-);
+)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.unregister()
